1b2a6ad9042d2bf5b640a1eddff7e9cd4079e383
EmilyPlatzer <emilyplatzer@gmail.com>
2014-07-01 09:56:16 -0600

    User can return an array of all names (recursively)

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,7 +1,24 @@
 require 'node'
 
 class Tree
+  def initialize
+    @names = []
+    @i = 0
+  end
 
+  def print_names(name_tree)
+    puts name_tree.name
+    name_tree.children.each do |child_node|
+      print_names(child_node)
+    end
+  end
 
+  def names(name_tree)
+    @names << name_tree.name.to_s
+    name_tree.children.each do |child_node|
+      names(child_node)
+    end
+    @names
+  end
 
 end
